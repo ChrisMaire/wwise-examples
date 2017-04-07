7824ae8d9abceae3ab0f571f9a2cf1164a884c04
locura.madness@gmail.com <locura.madness@gmail.com>
2017-04-06 23:12:47 -0400

    Initial checkin

---
+++ Assets/CharacterSoundPlayer.cs
@@ -0,0 +1,100 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using  System.Linq;
+
+[System.Serializable]
+public class TextureSwitchPair
+{
+    public string TextureName;
+    public string SwitchName;
+}
+
+public class CharacterSoundPlayer : MonoBehaviour
+{
+    public List<TextureSwitchPair> FootstepSwitches;
+    private Dictionary<string, string> switchesByTexture;
+
+    private float vol;
+    private TerrainData terrainData;
+    private Vector3 terrainPos;
+
+    void Start()
+    {
+        //create a dictionary mapping texture names to switch names
+        switchesByTexture = FootstepSwitches.ToDictionary(t => t.TextureName, s => s.SwitchName);
+
+        terrainData = Terrain.activeTerrain.terrainData;
+        terrainPos =  Terrain.activeTerrain.GetPosition();
+
+        vol = GetAndPrintVolume();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.Equals) || Input.GetKeyUp(KeyCode.Minus))
+        {
+            if (Input.GetKeyUp(KeyCode.Equals))
+            {
+                vol += 10;
+            }
+            else
+            {
+                vol -= 10;
+            }
+            AkSoundEngine.SetRTPCValue("Volume", vol + 10, null);
+
+            GetAndPrintVolume();
+        }
+    }
+
+    public float GetAndPrintVolume()
+    {
+        float v = 0.5f;
+        int vtype = (int)RTPCValue_type.RTPCValue_Global;
+        uint pid = (uint)RTPCValue_type.RTPCValue_PlayingID;
+        AkSoundEngine.GetRTPCValue("Volume", gameObject, pid, out v, ref vtype);
+
+        Debug.Log("volume is now " + v);
+
+        return v;
+    }
+
+    /// <summary>
+    /// Set the correct switch based on the floor, and play a footstep audio event.
+    /// </summary>
+    /// <param name="animEvent"></param>
+    public void PlayFootstep(AnimationEvent animEvent)
+    {
+        //Debug.Log(animEvent.animatorClipInfo.weight);
+
+        if (animEvent.animatorClipInfo.weight < 0.5)
+        {
+            Texture floorText = getTerrainTextureAt(transform.position);
+
+            AkSoundEngine.SetSwitch("Material", switchesByTexture[floorText.name], gameObject);
+            AkSoundEngine.PostEvent("sfx_footstep", gameObject);
+        }
+    }
+
+    //based on http://answers.unity3d.com/questions/14998/how-can-i-perform-some-action-based-on-the-terrain.html
+    public Texture getTerrainTextureAt(Vector3 position)
+    {
+        int AX = (int)(((transform.position.x - terrainPos.x) / terrainData.size.x) * terrainData.alphamapWidth);
+        int AZ = (int)(((transform.position.z - terrainPos.z) / terrainData.size.z) * terrainData.alphamapHeight);
+        float[,,] alphaMap = Terrain.activeTerrain.terrainData.GetAlphamaps(AX, AZ, 1, 1);
+
+        Texture retval = terrainData.splatPrototypes[0].texture;
+        int idx = 0;
+        for (int i = 0; i < terrainData.splatPrototypes.Length; i++)
+        {
+            if (alphaMap[0, 0, i] > alphaMap[0, 0, idx])
+            {
+                idx = i;
+                retval = terrainData.splatPrototypes[i].texture;
+            }
+        }
+
+        return retval;
+    }
+}
---
+++ Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -0,0 +1,53 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Audiokinetic Wwise generated include file. Do not edit.
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#ifndef __WWISE_IDS_H__
+#define __WWISE_IDS_H__
+
+#include <AK/SoundEngine/Common/AkTypes.h>
+
+namespace AK
+{
+    namespace EVENTS
+    {
+        static const AkUniqueID SFX_FOOTSTEP = 1069143671U;
+    } // namespace EVENTS
+
+    namespace SWITCHES
+    {
+        namespace MATERIAL
+        {
+            static const AkUniqueID GROUP = 3865314626U;
+
+            namespace SWITCH
+            {
+                static const AkUniqueID GRASS = 4248645337U;
+                static const AkUniqueID ROCK = 2144363834U;
+            } // namespace SWITCH
+        } // namespace MATERIAL
+
+    } // namespace SWITCHES
+
+    namespace GAME_PARAMETERS
+    {
+        static const AkUniqueID VOLUME = 2415836739U;
+    } // namespace GAME_PARAMETERS
+
+    namespace BANKS
+    {
+        static const AkUniqueID INIT = 1355168291U;
+        static const AkUniqueID MAINSOUNDBANK = 534561221U;
+    } // namespace BANKS
+
+    namespace BUSSES
+    {
+        static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
+        static const AkUniqueID MASTER_SECONDARY_BUS = 805203703U;
+    } // namespace BUSSES
+
+}// namespace AK
+
+#endif // __WWISE_IDS_H__
